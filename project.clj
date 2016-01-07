4f1012126b70d9acf0e290477e83e51fb4c315c4
Jeremy Anderson <jeremy@Kessel.local>
2016-01-06 20:10:35 -0500

    update cucumber version

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.punkisdead/lein-cucumber "1.0.4"
+(defproject org.clojars.punkisdead/lein-cucumber "1.0.5"
   :description "Run cucumber-jvm specifications with leiningen"
   :url https://github.com/punkisdead/lein-cucumber
-  :dependencies [[info.cukes/cucumber-clojure "1.2.0"]
+  :dependencies [[info.cukes/cucumber-clojure "1.2.4"]
                  [leiningen-core "2.0.0"]
                  [org.clojure/clojure "1.6.0"]]
   :profiles {:cucumber {:dependencies [[commons-io "2.4"]]
-                        :plugins [[org.clojars.punkisdead/lein-cucumber "1.0.4"]]}}
+                        :plugins [[org.clojars.punkisdead/lein-cucumber "1.0.5"]]}}
   :eval-in :leiningen
   :license {:name "Unlicense"
             :url "http://unlicense.org/"
